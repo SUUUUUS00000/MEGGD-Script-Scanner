511e62fe7adb3d42f35a8f8f1c1ce01848cede5c
SUUUUUS00000 <specialakk69@gmail.com>
2026-03-30 23:44:52 +0300

    Update script URLs for mobile and PC versions

--- LoadScript.lua
+++ LoadScript.lua
@@ -3,7 +3,7 @@ local uis = game:GetService("UserInputService")
 local is_mobile = uis.TouchEnabled and not uis.KeyboardEnabled
 
 if is_mobile then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AMOGUS392/MEGGD-Script-Scanner-Beta-Test-1.1.0/refs/heads/main/Device/MEGGD%20Script%20Scanner(Mobile).lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(Mobile).lua", true))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AMOGUS392/MEGGD-Script-Scanner-Beta-Test-1.1.0/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
 end
