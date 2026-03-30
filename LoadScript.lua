722e36424e71146bd671eb5b0c6a8764ca58ba44
SUUUUUS00000 <specialakk69@gmail.com>
2026-03-30 23:25:51 +0300

    Load different scripts based on device type

--- LoadScript.lua
+++ LoadScript.lua
@@ -1,1 +1,9 @@
+local uis = game:GetService("UserInputService")
 
+local is_mobile = uis.TouchEnabled and not uis.KeyboardEnabled
+
+if is_mobile then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AMOGUS392/MEGGD-Script-Scanner-Beta-Test-1.1.0/refs/heads/main/Device/MEGGD%20Script%20Scanner(Mobile).lua", true))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AMOGUS392/MEGGD-Script-Scanner-Beta-Test-1.1.0/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
+end
