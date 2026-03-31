c10cd379aeac123c14b30d53dfacf7ad3583c5be
SUUUUUS00000 <specialakk69@gmail.com>
2026-03-31 16:54:40 +0300

    Add loading message for script execution

--- LoadScript.lua
+++ LoadScript.lua
@@ -1,10 +1,9 @@
 local uis = game:GetService("UserInputService")
 
 local is_mobile = uis.TouchEnabled and not uis.KeyboardEnabled
-
+print("Loading...")
 if is_mobile then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(Mobile).lua", true))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/SUUUUUS00000/MEGGD-Script-Scanner/refs/heads/main/Device/MEGGD%20Script%20Scanner(PC).lua", true))()
 end
-print("Loading...")
